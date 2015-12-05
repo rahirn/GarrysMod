c5336c2a591133d10d4149dfa59d3ee9e602f2a0
Riley Hirn <rahirn@mtu.edu>
2015-12-05 15:20:07 -0500

    initial menu added

--- lua/autorun/client/Menu.lua
+++ lua/autorun/client/Menu.lua
@@ -13,11 +13,24 @@ function openMenu()
 	
 	local weaponModel = panel:CreateRow("Weapon", "Model")
 	weaponModel:Setup("Combo", {text = "Select Weapon Model..."})
-	weaponModel:AddChoise("one", {})
-	weaponModel:AddChoise("two", {})
-	weaponModel:AddChoise("three", {})
-	--weaponModel.DataChanged = function(self, data)
-		--print("You Selected: ", data)
+	weaponModel:AddChoice("one", {})
+	weaponModel:AddChoice("two", {})
+	weaponModel:AddChoice("three", {})
+	weaponModel.DataChanged = function(self, data)
+		print("You Selected: ", data)
+	end
+	
+	
+	local weaponModel = panel:CreateRow("Ammo", "Model")
+	weaponModel:Setup("Combo", {text = "Select Ammo Model..."})
+	weaponModel:AddChoice("first", {})
+	weaponModel:AddChoice("second", {})
+	weaponModel:AddChoice("third", {})
+	weaponModel.DataChanged = function(self, data)
+		print("You Selected: ", data)
+	end
+	
+	
 	end
 	
 concommand.Add("weaponMenu", openMenu) 
